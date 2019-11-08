01c10bcf4a38baf7f503727fc904018467f2c363
Lucas Aho <aholucas11@gmail.com>
2019-11-07 17:03:52 -0700

    Homework completed

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,32 +1,27 @@
 //Giphy category names in the Art and Design GIFs
-var topics = ["architecture","cinemagraph", "glitch","loop","mash up","pixel","sculpture","timelapse","typography","cats"];
-topics.forEach(function(element){
-    btnMaker(element);
-});
-function btnMaker(element) {
-    $("#btnDiv").append("<button class='gifMake' data-cat=" + element + "><span>"+element+"</span></button>");
-    
-}
+var topics = ["architecture","cinemagraph", "glitch","loop","mash up","pixel","sculpture","timelapse","typography"];
 
 function gifMake() {
-    var 
-}
-$(".gifMake").on("click", function() {   
     var artCat = $(this).attr("data-cat");
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + artCat + "&api_key=OxxX073XPGULlo72miD7LFTqtLFz13gP&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + artCat + "&api_key=OxxX073XPGULlo72miD7LFTqtLFz13gP&limit=10"; 
 
+// $(".gifMake").on("click", function() {   
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         var results = response.data;        
-
+        //Loops through results
         for (var i = 0; i < results.length; i++) {
+            //Creating divs to hold gifs and their attributes
             var gifDiv = $("<div>");
+            //Storing rating data
             var rating = results[i].rating;
+            //Creating element to hold rating
             var p = $("<p>").text("Rating: " + rating);
+            //Creating element to hold gif
             var artImg = $("<img>");
+            //Giving pause/play functionality to gifs
             artImg.attr({
                 "src": results[i].images.original_still.url,
                 "data-still": results[i].images.original_still.url,
@@ -34,39 +29,51 @@ $(".gifMake").on("click", function() {
                 "data-state": "still",
                 "class": "gif"
             });
+            //Updating html to variables
             gifDiv.append(p);
             gifDiv.append(artImg);
-            $("#images").prepend(gifDiv);
-            console.log(response);
+            $("#images").prepend(gifDiv);           
             
         }
-        
+        //Pause/Play functionality with gif click.
         $(".gif").on("click", function(){
-            console.log("clicked :D");
             var state = $(this).attr("data-state");
             if(state === "still") {
                 var newSrc = $(this).attr("data-animate");
                 $(this).attr("src", newSrc);
-                console.log("clicked :D");
                 $(this).attr("data-state","animate");
                 $("#images").prepend(gifDiv);
             } else {
                 var newSrc = $(this).attr("data-still");
                 $(this).attr("src", newSrc);
-                console.log("clicked :D");
                 $(this).attr("data-state","still");
                 $("#images").prepend(gifDiv);
             }
-         });
-
-        
+        });
     });
+    
+}
+//Displays buttons for each topic
+function btnRender(){
+    $("#btnDiv").empty();
 
+    //Loops through topics array
+    topics.forEach(function(element){
+    //Creates button named for and valued by the topics array
+        $("#btnDiv").append("<button class='gifMake' data-cat=" + element + "><span>"+element+"</span></button>");
+
+    });
+}
+
+
+$("#userSubmit").on("click", function(event){
+    event.preventDefault();
+
+    var artCat = $("#userInput").val().trim();
+
+    topics.push(artCat);
+
+    btnRender();
 });
-
-
-$("#userSubmit").on("click", function(){
-    var userIn = $("#userInput").val().trim();
-    console.log(topics);
-    btnMaker(userIn);
-});
+$(document).on("click", ".gifMake", gifMake);
+btnRender();
